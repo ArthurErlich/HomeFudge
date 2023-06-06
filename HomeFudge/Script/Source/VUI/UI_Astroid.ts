d8e7fa684aadb0e7e4c33b02e58b6ae21fb55410
ArthurErHFU <arthur.erlich@hs-furtwangen.de>
2023-06-06 19:20:50 +0200

    Removed difected function

--- HomeFudge/Script/Source/VUI/UI_Astroid.ts
+++ HomeFudge/Script/Source/VUI/UI_Astroid.ts
@@ -1,10 +1,8 @@
 namespace HomeFudge {
     import ƒ = FudgeCore;
     export class UI_Astroid extends ƒ.Mutable{
-        public selection:
-
         protected reduceMutator(_mutator: ƒ.Mutator): void {
+            throw new Error("Method not implemented.");
         }
-
     }
 }
