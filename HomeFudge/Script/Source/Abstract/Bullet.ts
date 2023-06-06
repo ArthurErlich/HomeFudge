faaa0225e02daeaccde731e192427f9322d559ff
ArthurErHFU <arthur.erlich@hs-furtwangen.de>
2023-06-06 19:18:52 +0200

    Removed unused text made it an GameObject

--- HomeFudge/Script/Source/Abstract/Bullet.ts
+++ HomeFudge/Script/Source/Abstract/Bullet.ts
@@ -1,14 +1,11 @@
+/// <reference path="GameObject.ts" />
 namespace HomeFudge {
-    import ƒ = FudgeCore;
-    /* This is a TypeScript class definition for an abstract class called `Bullet` that extends the
-    `ƒ.Node` class. The `export` keyword makes the class available for use in other modules. */
-    export abstract class Bullet extends ƒ.Node {
+    export abstract class Bullet extends GameObject {
         protected abstract maxLifeTime:number;
 
         //abstract faction:string; //may be used later for multiple turrets
         public abstract update():void;
         public abstract destroyNode():void; 
-        public abstract toString():string;
 
         constructor(idString:string){
             super("Bullet" + idString);
