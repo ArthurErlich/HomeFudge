a82c168f4d6fe319affeaa592b3c1cf2127b9900
ArthurErHFU <arthur.erlich@hs-furtwangen.de>
2023-06-13 02:51:03 +0200

    Added getMultiplyComponentNodes

--- HomeFudge/Script/Source/Resources.ts
+++ HomeFudge/Script/Source/Resources.ts
@@ -1,6 +1,6 @@
-namespace HomeFudge{
-    import ƒ=FudgeCore;
-    export class Resources{
+namespace HomeFudge {
+    import ƒ = FudgeCore;
+    export class Resources {
         public static async getGraphResources(graphID: string): Promise<ƒ.Graph> {
             let graph: ƒ.Graph = <ƒ.Graph>ƒ.Project.resources[graphID]
             if (graph == null) {
@@ -14,6 +14,19 @@ namespace HomeFudge{
                 console.warn("+\"" + nodeName + "\" not found inside: " + graph.name + "->Graph");
             }
             return node;
+        }
+        public static async getMultiplyComponentNodes(nodeNames: string[], graph: ƒ.Graph): Promise<ƒ.Node[]> {
+            let nodeList: ƒ.Node[] = new Array(nodeNames.length);
+            let index: number = 0;
+            nodeNames.forEach(name => {
+                nodeList[index] = graph.getChildrenByName(name)[0];
+                index++;
+            });
+
+            if (nodeList == null || nodeList[0] == undefined) {
+                console.warn("+\"" + nodeNames.toString() + "\" not found inside: " + graph.name + "->Graph");
+            }
+            return nodeList;
         }
     }
 }
