a0f5d9b0e4859f4193b0f7078a95603ca3860e26
ArthurErHFU <arthur.erlich@hs-furtwangen.de>
2023-06-13 02:52:08 +0200

    Debug, enabled Destroyers

--- HomeFudge/Script/Source/Main.ts
+++ HomeFudge/Script/Source/Main.ts
@@ -23,7 +23,7 @@ namespace HomeFudge {
   let p1: Player = null;
 
   ///Destroyer\\\
- 
+
 
 
 
@@ -59,23 +59,25 @@ namespace HomeFudge {
       _viewport.getBranch().addChild(p1);
       _mainCamera.attachToShip(p1.destroyer);
       /// ------------T-E-S-T--A-R-E-A------------------\\\
-      // let destroyer = new Destroyer(ƒ.Matrix4x4.TRANSLATION(new ƒ.Vector3(500, 0, 0)));
-      // let mtx:ƒ.Matrix4x4 = ƒ.Matrix4x4.TRANSLATION(new ƒ.Vector3(400, 30, 0));
-      // mtx.rotation =new ƒ.Vector3(0,90,0);
-      // let destroyer2 = new Destroyer(mtx);
-      // _worldNode.appendChild(destroyer2);
-      // _worldNode.appendChild(destroyer);
-      let node: ƒ.Node= new ƒ.Node("name");
-      let nodeMes = new ƒ.ComponentMesh(new ƒ.MeshSprite);
-      nodeMes.mtxPivot.scale(new ƒ.Vector3(200,200,200));
-      node.addComponent(nodeMes);
-      node.addComponent(new ƒ.ComponentMaterial(new ƒ.Material("lit",ƒ.ShaderLit)));
-      _worldNode.appendChild(node);
+      let destroyer = new Destroyer(ƒ.Matrix4x4.TRANSLATION(new ƒ.Vector3(500, 0, 0)));
+      let mtx: ƒ.Matrix4x4 = ƒ.Matrix4x4.TRANSLATION(new ƒ.Vector3(400, 30, 0));
+      mtx.rotation = new ƒ.Vector3(0, 90, 0);
+      let destroyer2 = new Destroyer(mtx);
+      _worldNode.appendChild(destroyer2);
+      _worldNode.appendChild(destroyer);
+      // let node: ƒ.Node= new ƒ.Node("name");
+      // let nodeMes = new ƒ.ComponentMesh(new ƒ.MeshSprite);
+      // nodeMes.mtxPivot.scale(new ƒ.Vector3(200,200,200));
+      // node.addComponent(nodeMes);
+      // node.addComponent(new ƒ.ComponentMaterial(new ƒ.Material("lit",ƒ.ShaderLit)));
+      // _worldNode.appendChild(node);
       /// ------------T-E-S-T--A-R-E-A------------------\\\
     }
 
     /// ------------T-E-S-T--A-R-E-A------------------\\\
     //TODO: Before the loop starts. Add an Game Menu draws on frame while updating
+    let astroid = Astroid.spawn(new ƒ.Vector3(100, 0, -50), Astroid.getLarge());
+
     /// ------------T-E-S-T--A-R-E-A------------------\\\
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
