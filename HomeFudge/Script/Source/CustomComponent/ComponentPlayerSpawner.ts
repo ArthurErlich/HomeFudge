f1d1f2d35c26b331c13f6da817ab7085b75c3dbf
ArthurErHFU <arthur.erlich@hs-furtwangen.de>
2023-06-18 20:12:18 +0200

    Renamend and moved Components

--- HomeFudge/Script/Source/CustomComponent/ComponentPlayerSpawner.ts
+++ HomeFudge/Script/Source/CustomComponent/ComponentPlayerSpawner.ts
@@ -1,10 +1,10 @@
 namespace HomeFudge {
     import ƒ = FudgeCore;
-    ƒ.Project.registerScriptNamespace(Script);  // Register the namespace to FUDGE for serialization
+    ƒ.Project.registerScriptNamespace(HomeFudge);  // Register the namespace to FUDGE for serialization
 
-    export class PlayerSpawnerComponent extends ƒ.ComponentScript {
+    export class ComponentPlayerSpawner extends ƒ.ComponentScript {
         // Register the script as component for use in the editor via drag&drop
-        public static readonly iSubclass: number = ƒ.Component.registerSubclass(PlayerSpawnerComponent);
+        public static readonly iSubclass: number = ƒ.Component.registerSubclass(ComponentPlayerSpawner);
         // Properties may be mutated by users in the editor via the automatically created user interface
         
 
